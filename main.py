73b8ea3ddc048c29143e219d0241ebc424d9565e
Asegai <144498317+Asegai@users.noreply.github.com>
2024-06-17 23:05:34 +0100

    centered button

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.relativelayout import RelativeLayout
 
 class class_Test(App):
     def build(self):
-        main_layout = BoxLayout(orientation='vertical', padding=20, spacing=20)
+        main_layout = RelativeLayout()
 
-        label = Label(text='Hello the World', size_hint=(1, 0.8))  
-        
-        button_layout = BoxLayout(orientation='horizontal', size_hint=(1, None)) 
-        
-        # the button isn't centered
-        #! Center button!
-    
+        # larger font size
+        label = Label(text='Hello the World', font_size=24, size_hint=(None, None), size=(300, 100),
+                      pos_hint={'center_x': 0.5, 'center_y': 0.7}) 
 
-        button = Button(text='World', size_hint=(None, None), size=(100, 50))
-
-        button_layout.add_widget(BoxLayout())  
-        button_layout.add_widget(button)
-        button_layout.add_widget(BoxLayout())  
+        # Button right under the label, centered
+        button = Button(text='World', size_hint=(None, None), size=(150, 40),
+                        pos_hint={'center_x': 0.5, 'center_y': 0.5})  # Centered
 
         main_layout.add_widget(label)
-        main_layout.add_widget(button_layout)
+        main_layout.add_widget(button)
 
         return main_layout
 
