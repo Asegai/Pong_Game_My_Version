73a6e468935a3bd8c9366dd6c1d6e24ee2b2011d
Asegai <144498317+Asegai@users.noreply.github.com>
2024-06-18 17:09:07 +0100

    score added
    ball bounces now and there's a score board

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProper
 from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.lang import Builder
+import random
 
 Builder.load_string('''
 <PongBall>:
-    size: 30, 30
+    size: 50, 50
     canvas:
         Ellipse:
             pos: self.pos
@@ -27,10 +28,12 @@ Builder.load_string('''
     ball: pong_ball
     player1: left_paddle
     player2: right_paddle
+    player1_score: player1_score_label
+    player2_score: player2_score_label
 
     PongBall:
         id: pong_ball
-        center: self.parent.center
+        center: self.center
 
     PongPaddle:
         id: left_paddle
@@ -41,6 +44,20 @@ Builder.load_string('''
         id: right_paddle
         x: root.width - self.width
         center_y: root.center_y
+
+    Label:
+        id: player1_score_label
+        text: str(root.player1.score)
+        font_size: 30
+        center_x: root.width / 4
+        top: root.top - 10
+
+    Label:
+        id: player2_score_label
+        text: str(root.player2.score)
+        font_size: 30
+        center_x: root.width * 3 / 4
+        top: root.top - 10
 ''')
 
 class PongBall(Widget):
@@ -56,12 +73,18 @@ class PongPaddle(Widget):
 
     def bounce_ball(self, ball):
         if self.collide_widget(ball):
-            ball.velocity_x *= -1.1
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced * 1.1
+            ball.velocity = vel.x, vel.y + offset
 
 class PongGame(Widget):
     ball = ObjectProperty(None)
     player1 = ObjectProperty(None)
     player2 = ObjectProperty(None)
+    player1_score = ObjectProperty(None)
+    player2_score = ObjectProperty(None)
 
     def serve_ball(self, vel=(4, 0)):
         self.ball.center = self.center
@@ -70,15 +93,15 @@ class PongGame(Widget):
     def update(self, dt):
         self.ball.move()
 
-        # bounce  padles
+        # bounce off paddles
         self.player1.bounce_ball(self.ball)
         self.player2.bounce_ball(self.ball)
 
-        
+        # bounce off top and bottom
         if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.velocity_y *= -1
 
-        
+        # went off to a side to score point?
         if self.ball.x < 0:
             self.player2.score += 1
             self.serve_ball(vel=(4, 0))
@@ -86,11 +109,18 @@ class PongGame(Widget):
             self.player1.score += 1
             self.serve_ball(vel=(-4, 0))
 
+        self.player1_score.text = str(self.player1.score)
+        self.player2_score.text = str(self.player2.score)
+
+        # AI for the right paddle
+        if self.ball.center_y > self.player2.center_y:
+            self.player2.center_y += min(self.ball.center_y - self.player2.center_y, 4)
+        if self.ball.center_y < self.player2.center_y:
+            self.player2.center_y -= min(self.player2.center_y - self.ball.center_y, 4)
+
     def on_touch_move(self, touch):
         if touch.x < self.width / 3:
             self.player1.center_y = touch.y
-        if touch.x > self.width * 2 / 3:
-            self.player2.center_y = touch.y
 
 class MainMenu(RelativeLayout):
     def __init__(self, **kwargs):
@@ -117,8 +147,3 @@ class PongApp(App):
 
 if __name__ == '__main__':
     PongApp().run()
-
-
-
-# https://kivy.org/doc/stable/tutorials/pong.html
-# 
